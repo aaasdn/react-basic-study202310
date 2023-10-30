6bcb7fa15406aeed2a713e8dc9be8e972a8915fd
songseungkeon <song76543@naver.com>
2023-10-30 15:50:10 +0900

    1.React 초기설정 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Expenseltem from './components/Expenseltem';
+import NoName from './NoName';
 
 function App() {
+  const $h2 = <h2>반가워요~</h2>;
+
+  const value = '메롱메롱';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+    <Expenseltem />
+    <Expenseltem />
+    <Expenseltem />
+    <Expenseltem />
+    <Expenseltem />
+    <Expenseltem />
+      <div className="App">
+        <h1>메롱메롱</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        오늘은 월요일 입니다. <br />
+        그래서 공부하기가 싫어요~
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
