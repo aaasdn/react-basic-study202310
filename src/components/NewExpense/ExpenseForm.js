58c3b20011fa8b5bc9eaacc6a2a9eeba19ce282a
songseungkeon <song76543@naver.com>
2023-11-02 16:09:41 +0900

    20231102 react-study

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = ({ onSaveExpense }) => {
+const ExpenseForm = ({ onSaveExpense, onToggle }) => {
   const [userInput, setUserInput] = useState({
     title: '',
     price: '',
@@ -33,10 +33,14 @@ const ExpenseForm = ({ onSaveExpense }) => {
 
   const formSubmitHandler = (e) => {
     e.preventDefault(); // submit 차단
-    console.log('submit 버튼을 누름!');
-    console.log(userInput);
 
-    onSaveExpense();
+    const newExpense = {
+      title: userInput.title,
+      price: userInput.price,
+      date: new Date(userInput.date),
+    };
+
+    onSaveExpense(newExpense);
 
     // 입력창 리셋
     setUserInput({
@@ -44,6 +48,13 @@ const ExpenseForm = ({ onSaveExpense }) => {
       price: '',
       date: '',
     });
+
+    onToggle();
+  };
+
+  const cancelInsertHandler = () => {
+    console.log('취소 버튼 누름!');
+    onToggle();
   };
 
   return (
@@ -79,6 +90,12 @@ const ExpenseForm = ({ onSaveExpense }) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button
+          type='button'
+          onClick={cancelInsertHandler}
+        >
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
