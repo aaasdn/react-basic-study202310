4e53033549d6708ec0c88ce8a8f0a5405c0d458c
songseungkeon <song76543@naver.com>
2023-11-15 13:00:02 +0900

    Cart 화면에서 마이너스 버튼 처리

--- src/components/Food/Cart/CartItem.js
+++ src/components/Food/Cart/CartItem.js
@@ -4,9 +4,9 @@ import styles from './CartItem.module.scss';
 import CartContext from '../../../store/cart-context';
 
 const CartItem = ({ cart }) => {
-  const { name, price, amount } = cart;
+  const { id, name, price, amount } = cart;
 
-  const { addItem } = useContext(CartContext);
+  const { addItem, removeItem } = useContext(CartContext);
 
   const {
     'cart-item': cartItem,
@@ -17,8 +17,13 @@ const CartItem = ({ cart }) => {
   } = styles;
   const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
+  // + 버튼 누르면 무적권 amount는 하나다!
   const cartAddItemHandler = () => {
     addItem({ ...cart, amount: 1 });
+  };
+
+  const cartRemoveItemHandler = () => {
+    removeItem(id);
   };
 
   return (
@@ -31,7 +36,7 @@ const CartItem = ({ cart }) => {
         </div>
       </div>
       <div className={actions}>
-        <button>−</button>
+        <button onClick={cartRemoveItemHandler}>−</button>
         <button onClick={cartAddItemHandler}>+</button>
       </div>
     </li>
